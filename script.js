e615e9cca80a88216510ff681ece11a0ba879e41
Alexandru <anistor35@yahoo.com>
2023-08-24 16:26:05 +0300

    Arranged the structure of the page

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-function change(){
-   gridDiv.style.color = "red";
-};
+const grid = document.getElementById("grid-container")
+const colorBtn = document.getElementById("color")
+const rainbowBtn = document.getElementById("rainbow")
+const eraseBtn = document.getElementById("erase")
+const resetBtn = document.getElementById("reset")
+const slider = document.getElementById("range-slider")
+let sliderValue = document.getElementById("range-value")
+const DEFAULT_SIZE = 16
+
 
 function setupGrid(size){
-    const grid = document.getElementById("grid-container");
+    
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
     for(let i = 0; i < size * size; i++){
-        const gridDiv = document.createElement("div");
+        const gridDiv = document.createElement("div")
+        gridDiv.addEventListener("mouseover", function(){
+            gridDiv.style.backgroundColor = "red"
+        })
         grid.appendChild(gridDiv);
     }
   
 }
 
-function ColorMode(){
-    grid = addEventListener(onmouseover, change);
+resetBtn.onclick = () => resetGrid()
+slider.onmousemove = (e) => setupGrid(e.target.value)
+slider.onchange = (e) => updateSize(e.target.value)
 
+function resetGrid(){
+    grid.innerHTML = ""
+    setupGrid(DEFAULT_SIZE)
+    updateSize(DEFAULT_SIZE)
 }
 
+function updateSize(value){
+    sliderValue.innerHTML = `Grid size: ${value} x ${value}` 
+}
 
-
-
-
-
-window.onload = () => setupGrid(30  );
+window.onload = () => setupGrid(16);
