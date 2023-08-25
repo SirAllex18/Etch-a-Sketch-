e68293b170861f760b7a63b44321410241f88363
Alexandru <anistor35@yahoo.com>
2023-08-25 10:51:35 +0300

    Added functions to control the color mode

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const rainbowBtn = document.getElementById("rainbow")
 const eraseBtn = document.getElementById("erase")
 const resetBtn = document.getElementById("reset")
 const slider = document.getElementById("range-slider")
-let sliderValue = document.getElementById("range-value")
 const DEFAULT_SIZE = 16
-
+let sliderValue = document.getElementById("range-value")
+let current_mode = "color"
 
 function setupGrid(size){
     
@@ -15,10 +15,9 @@ function setupGrid(size){
 
     for(let i = 0; i < size * size; i++){
         const gridDiv = document.createElement("div")
-        gridDiv.addEventListener("mouseover", function(){
-            gridDiv.style.backgroundColor = "red"
-        })
+        gridDiv.addEventListener("mouseover", changeColor)    
         grid.appendChild(gridDiv);
+        
     }
   
 }
@@ -26,6 +25,33 @@ function setupGrid(size){
 resetBtn.onclick = () => resetGrid()
 slider.onmousemove = (e) => setupGrid(e.target.value)
 slider.onchange = (e) => updateSize(e.target.value)
+colorBtn.onclick = () => updateMode("color")
+rainbowBtn.onclick = () => updateMode("rainbow")
+eraseBtn.onclick = () => updateMode("erase")
+
+
+function updateMode(newMode){
+    if(newMode === "color"){
+        current_mode = "color"
+    }else if(newMode === "rainbow"){
+        current_mode = "rainbow"
+    }else if(newMode === "erase"){
+        current_mode = "erase"
+    }
+}
+
+function changeColor(){
+    if(current_mode === "color"){
+        this.style.backgroundColor = "red"
+    }else if(current_mode === "rainbow"){
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        this.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    }else if(current_mode === "erase"){
+        this.style.backgroundColor = "white";
+    }
+}
 
 function resetGrid(){
     grid.innerHTML = ""
